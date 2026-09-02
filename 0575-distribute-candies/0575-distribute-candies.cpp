9d8a7f8e487da8cfb6330900195b7739408d036e
ANKIT CHOUDHARY <ankit16x2choudharytech@gmail.com>
2026-09-02 23:53:45 +0530

    Time: 18 ms (98.33%), Space: 85.6 MB (92.37%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.cpp
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -6,14 +6,14 @@ public:
         sort(candyType.begin(), candyType.end());
 
         int unique = 1;
-        int candy;
+        
 
         for(int i = 1; i < n; i++) {
             if(candyType[i] != candyType[i - 1]) {
                 unique++;
             }
         }
-                candy =min(n / 2, unique);
-        return  candy;
+                
+        return  min(n / 2, unique);
     }
 };
