34aa393f9ad901e69b91e463a4ed59340bc57a61
ANKIT CHOUDHARY <ankit16x2choudharytech@gmail.com>
2026-09-18 09:48:37 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+int climbstairs(int n){
+      vector<int> dp(n+2,-1);
+      dp[0]=0;
+      dp[1]=1;
+      dp[2]=2;
+      if(dp[n] != -1)
+          return dp[n];
+      for(int i =3;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+      }
+      return dp[n];
+    
+}
+    int climbStairs(int n) {
+       
+        return climbstairs(n);
+        
+    }
+};
